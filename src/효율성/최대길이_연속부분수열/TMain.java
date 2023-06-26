cf4a4d0ae2220d06bfa3687c5477873ae78376ec
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-06-26 14:43:40 +0900

    효율성6

---
+++ src/효율성/최대길이_연속부분수열/TMain.java
@@ -0,0 +1,29 @@
+package 효율성.최대길이_연속부분수열;
+
+import java.util.*;
+public class TMain {
+    public int solution(int n, int k, int[] arr){
+        int answer =0, cnt=0, lt=0;
+
+        for(int rt=0;rt<n;rt++){
+            if(arr[rt]==0) cnt++;
+            while(cnt>k){
+                if(arr[lt]==0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        TMain T = new TMain();
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <n; i++) {
+             arr[i] = scn.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+     }
+}
