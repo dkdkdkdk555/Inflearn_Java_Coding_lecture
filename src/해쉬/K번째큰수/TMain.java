2b172147fe087036b7952ea87169905010c54df5
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-07-05 18:52:51 +0900

    해쉬5

---
+++ src/해쉬/K번째큰수/TMain.java
@@ -0,0 +1,37 @@
+package 해쉬.K번째큰수;
+import java.util.*;
+public class TMain {
+    public int solution(int[] arr, int n, int k){
+        int answer=-1;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int l = j+1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        for(int x : Tset){
+            cnt++;
+            if(cnt==k) return x;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        TMain T = new TMain();
+        Scanner scn = new Scanner(System.in);
+        int n = 0;
+        int k = 0;
+        for (int i = 0; i < 2; i++) {
+            if(i==0) n = scn.nextInt();
+            else k = scn.nextInt();
+        }
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        System.out.println(T.solution(arr, n, k));
+    }
+}
