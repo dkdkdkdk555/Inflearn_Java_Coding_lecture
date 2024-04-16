9f9883aeec9b047f1ac140650a85bce31c791096
dkdkdkdk555 <dkdkdkdk555@naver.com>
2024-04-16 10:12:11 +0900

    복습 중

--- src/복습/Efficiency.java
+++ src/복습/Efficiency.java
@@ -1,11 +1,11 @@
 package 복습;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Efficiency {
 
-    public static void main(String[] args) {
+    // 1. 두 배열 합치기
+    public static void main1(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] nrr = new int[n];
@@ -35,4 +35,43 @@ public class Efficiency {
             System.out.print(i + " ");
         }
     }
+
+    // 2. 공통원소 구하기
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] nrr = new int[n];
+        for (int i = 0; i < n; i++) {
+            nrr[i] = scn.nextInt();
+        }
+        int m = scn.nextInt();
+        int[] mrr = new int[m];
+        for (int i = 0; i < m; i++) {
+            mrr[i] = scn.nextInt();
+        }
+
+        Arrays.sort(nrr);
+        Arrays.sort(mrr);
+
+        List<Integer> arr = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+
+        while (p1<n && p2 < m){
+            if(nrr[p1] == mrr[p2]){
+                arr.add(nrr[p1++]); // 추가한다음에 p1증가
+                p2++; // 오름차순으로 정렬을 해뒀으니 다음 nrr[p1]은 무조건 현재 mrr[p2] 보다 큼, 그러므로 증가시켜야함
+            } else if (nrr[p1] < mrr[p2]){
+                p1++; // 자기보다 큰것들과 비교할 필요 없으니 자신의 반복자를 증가시킴
+            } else if (nrr[p1] > mrr[p2]){
+                p2++; // 자기보다 작은것과 비교할 필요 없으니 비교군의 반복자를 증가시킴
+            }
+        }
+
+        for (Integer integer : arr) {
+            System.out.print(integer + " ");
+        }
+
+
+    }
+
 }
